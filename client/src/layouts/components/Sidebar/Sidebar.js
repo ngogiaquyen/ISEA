cfc773923efdae196acbdc99fbefb43509495ceb
levantien99 <levantien291124@gmail.com>
2024-12-17 22:16:33 +0700

    icon

--- client/src/layouts/components/Sidebar/Sidebar.js
+++ client/src/layouts/components/Sidebar/Sidebar.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MenuItem from "./MenuItem";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faAddressBook, faBell, faChartLine, faChartSimple, faClipboard, faCreditCard, faGear, faHome, faReceipt, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 import classNames from "classnames/bind";
 import styles from "./Sidebar.module.scss";
@@ -10,12 +10,12 @@ const cx = classNames.bind(styles);
 function Silebar() {
   return (
     <div className={cx("wrapper")}>
-      <MenuItem title="Tuyển dụng" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
-      <MenuItem title="Đánh giá hiệu suất" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
-      <MenuItem title="Lương và phúc lợi" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
-      <MenuItem title="Hồ sơ nhân viên" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
-      <MenuItem title="Báo cáo và thống kê" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
-      <MenuItem title="Cài đặt hệ thống" icon={ <FontAwesomeIcon className={cx("icon")} icon={faHome} /> }/>
+      <MenuItem title="Tuyển dụng" icon={ <FontAwesomeIcon className={cx("icon")} icon={faUserGroup} /> }/>
+      <MenuItem title="Đánh giá hiệu suất" icon={ <FontAwesomeIcon className={cx("icon")} icon={faChartLine} /> }/>
+      <MenuItem title="Lương và phúc lợi" icon={ <FontAwesomeIcon className={cx("icon")} icon={faReceipt} /> }/>
+      <MenuItem title="Hồ sơ nhân viên" icon={ <FontAwesomeIcon className={cx("icon")} icon={faAddressBook} /> }/>
+      <MenuItem title="Báo cáo và thống kê" icon={ <FontAwesomeIcon className={cx("icon")} icon={faChartSimple} /> }/>
+      <MenuItem title="Cài đặt hệ thống" icon={ <FontAwesomeIcon className={cx("icon")} icon={faGear} /> }/>
     </div>
   );
 }
