55cc83715eb4ae03850a6d63c5fa2296fa7e8e10
levantien99 <levantien291124@gmail.com>
2025-03-11 17:24:14 +0700

    cap nhap khoa dao tao

--- client/src/layouts/components/Form/TrainingForm.js
+++ client/src/layouts/components/Form/TrainingForm.js
@@ -4,7 +4,9 @@ import FormGroup from '~/components/FormGroup';
 import { isNotEmpty, isValidEmail, isValidPhoneNumber } from '~/hooks/validate';
 
 const cx = classNames.bind(styles);
-
+const isValidInstructorName = (value) => {
+  return /^[A-Za-zÀ-ỹ\s]+$/.test(value.trim()); // Chỉ cho phép chữ cái và khoảng trắng
+};
 function TrainingForm({ ref, data }) {
   return (
     <form className={cx('form')} ref={ref}>
@@ -12,18 +14,26 @@ function TrainingForm({ ref, data }) {
         name=""
         label="Tên khóa học"
         placeholder="Tên khóa học"
-        handleValidate={[{ funct: isNotEmpty, message: 'Vui lòng nhập tên khóa học!' }]}
+        handleValidate={[
+          { funct: isNotEmpty, message: 'Vui lòng nhập tên khóa học!' },
+          { funct: isValidInstructorName, message: 'Tên khóa học không được chứa số hoặc ký tự đặc biệt!'  }
+        ]}
       />
+      
       <FormGroup
         name="instructor"
         label="Giảng viên"
         layout="haft"
         inputType="text"
         placeholder="Giảng viên"
-        handleValidate={[{ funct: isNotEmpty, message: 'Vui lòng nhập tên giảng iảng viên!' }]}
+        handleValidate={[
+          { funct: isNotEmpty, message: 'Vui lòng nhập tên giảng viên!' },
+          { funct: isValidInstructorName, message: 'Tên giảng viên không được chứa số hoặc ký tự đặc biệt!'  }
+        ]}
+
       />
 
-      <FormGroup
+      {/* <FormGroup
         name=""
         label="Thông tin liên hệ"
         layout="haft"
@@ -32,52 +42,58 @@ function TrainingForm({ ref, data }) {
         handleValidate={[
           { funct: isNotEmpty, message: 'Vui lòng nhập thông tin liên hệ!' },
         ]}
-      />
-      <FormGroup
+      /> */}
+      {<FormGroup
         name=""
-        label="Ngày bắt đầu"
+        label="Thời gian bắt đầu"
         layout="haft"
         inputType="date"
-        placeholder="Ngày bắt đầu"
+        placeholder="Thời gian bắt đầu"
         handleValidate={[
           { funct: isNotEmpty, message: 'Vui lòng nhập ngày bắt đầu!' },
         ]}
-      />
+      /> }
       <FormGroup
         name=""
-        label="Ngày kết thúc"
+        label="Thời gian kết thúc"
         layout="haft"
         inputType="date"
-        placeholder="Ngày kết thúc"
+        placeholder="Thời gian kết thúc"
         handleValidate={[
           { funct: isNotEmpty, message: 'Vui lòng nhập ngày kết thúc!' },
         ]}
       />
-      <FormGroup
+      {/*<FormGroup
         name="interview_type"
         label="Hình thức"
         layout="haft"
         placeholder="Thời lượng"
         handleValidate={[{ funct: isNotEmpty, message: 'Vui lòng nhập thời lượng!' }]}
-      />
-      <FormGroup
+      /> */}
+      { <FormGroup
         name=""
         label="Địa điểm"
         layout="haft"
         inputType="text"
         placeholder="Địa điểm"
-        handleValidate={[{ funct: isNotEmpty, message: 'Vui lòng nhập địa điểm!' }]}
-      />
+        handleValidate={[
+          { funct: isNotEmpty, message: 'Vui lòng nhập địa điểm!' },
+        { funct: isValidInstructorName, message: 'Địa điểm không được chứa ký tự đặc biệt!'  }
+        ]}
+      /> }
       {/* <Dropdown dropDownItems={dropDownItems} placeholder="Người phỏng vấn" /> */}
       <FormGroup
         name="required_documents"
-        label="Mục tiêu"
+        label="Nội dung khoá học"
         layout="haft"
         textarea
         placeholder="Mục tiêu"
-        handleValidate={[{ funct: isNotEmpty, message: 'Vui lòng nhập mục tiêu!' }]}
+        handleValidate={[
+          { funct: isNotEmpty, message: 'Vui lòng nhập mục tiêu!' },
+        { funct: isValidInstructorName, message: 'Nội dung không được chứa ký tự đặc biệt!'  }
+        ]}
       />
-      <FormGroup name="note" label="Mô tả khóa học" layout="haft" textarea placeholder="Mô tả khóa học" />
+      {/* <FormGroup name="note" label="Mô tả khóa học" layout="haft" textarea placeholder="Mô tả khóa học" /> */}
     </form>
   );
 }
